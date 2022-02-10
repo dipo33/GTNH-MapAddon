1f60245bc02bd67e358b20821a15dffc798c5236
Matej Dipčár <mato.dipcar@gmail.com>
2022-02-10 19:20:47 +0100

    Add "Show Mined Chunks" button

--- src/main/java/io/github/dipo33/gtmapaddon/CommonProxy.java
+++ src/main/java/io/github/dipo33/gtmapaddon/CommonProxy.java
@@ -1,12 +1,22 @@
 package io.github.dipo33.gtmapaddon;
 
-import cpw.mods.fml.common.event.*;
+import com.sinthoras.visualprospecting.VisualProspecting_API;
+import com.sinthoras.visualprospecting.integration.journeymap.buttons.LayerButton;
+import com.sinthoras.visualprospecting.integration.model.buttons.ButtonManager;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerAboutToStartEvent;
+import cpw.mods.fml.common.event.FMLServerStartedEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.event.FMLServerStoppedEvent;
+import cpw.mods.fml.common.event.FMLServerStoppingEvent;
 
 public class CommonProxy {
 
     // preInit "Run before anything else. Read your config, create blocks, items,
     // etc, and register them with the GameRegistry."
-    public void preInit(FMLPreInitializationEvent event) 	{
+    public void preInit(FMLPreInitializationEvent event) {
         Config.syncronizeConfiguration(event.getSuggestedConfigurationFile());
 
         GTMapAddonMod.info(Config.greeting);
@@ -20,7 +30,11 @@ public class CommonProxy {
 
     // postInit "Handle interaction with other mods, complete your setup based on this."
     public void postInit(FMLPostInitializationEvent event) {
+        ButtonManager minedChunksButtonManager = new ButtonManager(Reference.MODID + ".button.minedChunks", "iconMinedChunks");
+        LayerButton minedChunksButton = new LayerButton(minedChunksButtonManager);
 
+        VisualProspecting_API.LogicalClient.registerCustomButtonManager(minedChunksButtonManager);
+        VisualProspecting_API.LogicalClient.registerJourneyMapButton(minedChunksButton);
     }
 
     public void serverAboutToStart(FMLServerAboutToStartEvent event) {
