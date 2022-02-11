b2e497ca7566ace76f0c67b3b46f1d9393b39f63
Matej Dipčár <mato.dipcar@gmail.com>
2022-02-11 11:58:16 +0100

    Refactor: Reformat `CommonProxy`

--- src/main/java/io/github/dipo33/gtmapaddon/CommonProxy.java
+++ src/main/java/io/github/dipo33/gtmapaddon/CommonProxy.java
@@ -21,32 +21,25 @@ public class CommonProxy {
 
     // load "Do your mod setup. Build whatever data structures you care about. Register recipes."
     public void init(FMLInitializationEvent event) {
-
     }
 
     // postInit "Handle interaction with other mods, complete your setup based on this."
     public void postInit(FMLPostInitializationEvent event) {
-
     }
 
     public void serverAboutToStart(FMLServerAboutToStartEvent event) {
-
     }
 
     // register server commands in this event handler
     public void serverStarting(FMLServerStartingEvent event) {
-
     }
 
     public void serverStarted(FMLServerStartedEvent event) {
-
     }
 
     public void serverStopping(FMLServerStoppingEvent event) {
-
     }
 
     public void serverStopped(FMLServerStoppedEvent event) {
-
     }
 }
