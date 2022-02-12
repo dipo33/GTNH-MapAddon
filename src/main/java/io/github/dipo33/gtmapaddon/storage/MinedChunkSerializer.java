2fc21e0933183c6d2f6e7db232dee49c6831a881
Matej Dipčár <mato.dipcar@gmail.com>
2022-02-12 22:29:42 +0100

    Refactor: Create an inner class `Keys` with key constants

--- src/main/java/io/github/dipo33/gtmapaddon/storage/MinedChunkSerializer.java
+++ src/main/java/io/github/dipo33/gtmapaddon/storage/MinedChunkSerializer.java
@@ -13,6 +13,14 @@ import java.util.List;
 
 public class MinedChunkSerializer {
 
+    private static class Keys {
+        public static final String CHUNK_X = "X";
+        public static final String CHUNK_Z = "Z";
+        public static final String MINED_BY = "By";
+        public static final String DIMENSION_IDS = "DimensionIDs";
+        public static final String DIMENSION = "DIM";
+    }
+
     private static NBTTagCompound serialize(DimensionStorage<MinedChunk> dimensionStorage) {
         NBTTagCompound dataTag = new NBTTagCompound();
         List<Integer> dimIds = new ArrayList<>();
@@ -20,18 +28,18 @@ public class MinedChunkSerializer {
             NBTTagList chunksTag = new NBTTagList();
             for (MinedChunk minedChunk : chunkStorage.getAll()) {
                 NBTTagCompound chunkTag = new NBTTagCompound();
-                chunkTag.setInteger("X", minedChunk.getChunkX());
-                chunkTag.setInteger("Z", minedChunk.getChunkZ());
-                chunkTag.setString("By", minedChunk.getMinedBy());
+                chunkTag.setInteger(Keys.CHUNK_X, minedChunk.getChunkX());
+                chunkTag.setInteger(Keys.CHUNK_Z, minedChunk.getChunkZ());
+                chunkTag.setString(Keys.MINED_BY, minedChunk.getMinedBy());
 
                 chunksTag.appendTag(chunkTag);
             }
 
-            dataTag.setTag("DIM" + chunkStorage.getDimensionId(), chunksTag);
+            dataTag.setTag(Keys.DIMENSION + chunkStorage.getDimensionId(), chunksTag);
             dimIds.add(chunkStorage.getDimensionId());
         }
 
-        dataTag.setIntArray("DimensionIDs", dimIds.stream().mapToInt(Integer::intValue).toArray());
+        dataTag.setIntArray(Keys.DIMENSION_IDS, dimIds.stream().mapToInt(Integer::intValue).toArray());
         return dataTag;
     }
 
