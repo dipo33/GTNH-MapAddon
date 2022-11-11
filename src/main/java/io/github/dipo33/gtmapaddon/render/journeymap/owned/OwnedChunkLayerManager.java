5dfea585f51521c5e9c1d2d649e49fcf12d87224
Matej Dipčár <492666@mail.muni.cz>
2022-11-11 08:41:09 +0100

    Dynamically display available owned chunks

--- src/main/java/io/github/dipo33/gtmapaddon/render/journeymap/owned/OwnedChunkLayerManager.java
+++ src/main/java/io/github/dipo33/gtmapaddon/render/journeymap/owned/OwnedChunkLayerManager.java
@@ -1,9 +1,13 @@
 package io.github.dipo33.gtmapaddon.render.journeymap.owned;
 
+import com.sinthoras.visualprospecting.Utils;
 import com.sinthoras.visualprospecting.integration.model.layers.WaypointProviderManager;
 import com.sinthoras.visualprospecting.integration.model.locations.IWaypointAndLocationProvider;
 import io.github.dipo33.gtmapaddon.ClientProxy;
+import io.github.dipo33.gtmapaddon.storage.ChunkStorage;
+import io.github.dipo33.gtmapaddon.storage.DataCache;
 import io.github.dipo33.gtmapaddon.storage.owned.OwnedChunk;
+import net.minecraft.client.Minecraft;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,21 +22,21 @@ public class OwnedChunkLayerManager extends WaypointProviderManager {
 
     @Override
     protected List<? extends IWaypointAndLocationProvider> generateVisibleElements(int minBlockX, int minBlockZ, int maxBlockX, int maxBlockZ) {
+        final int minOwnedChunkX = Utils.coordBlockToChunk(minBlockX);
+        final int minOwnedChunkZ = Utils.coordBlockToChunk(minBlockZ);
+        final int maxOwnedChunkX = Utils.coordBlockToChunk(maxBlockX);
+        final int maxOwnedChunkZ = Utils.coordBlockToChunk(maxBlockZ);
+        final ChunkStorage<OwnedChunk> chunkStorage = DataCache.OWNED_CHUNKS_STORAGE.getDimension(Minecraft.getMinecraft().thePlayer.dimension);
+        
         final List<OwnedChunkLocationProvider> locations = new ArrayList<>();
-        locations.add(new OwnedChunkLocationProvider(new OwnedChunk(0, 0, 0, "psuchtak", OwnedChunk.Status.BUILD)));
-        locations.add(new OwnedChunkLocationProvider(new OwnedChunk(1, 0, 0, "DirtyFaced", OwnedChunk.Status.BUILD)));
-        locations.add(new OwnedChunkLocationProvider(new OwnedChunk(2, 0, 0, "RainDrop_x", OwnedChunk.Status.BUILD)));
-        locations.add(new OwnedChunkLocationProvider(new OwnedChunk(3, 0, 0, "fb", OwnedChunk.Status.BUILD)));
-
-        locations.add(new OwnedChunkLocationProvider(new OwnedChunk(0, 1, 0, "psuchtak", OwnedChunk.Status.FARM)));
-        locations.add(new OwnedChunkLocationProvider(new OwnedChunk(1, 1, 0, "DirtyFaced", OwnedChunk.Status.FARM)));
-        locations.add(new OwnedChunkLocationProvider(new OwnedChunk(2, 1, 0, "RainDrop_x", OwnedChunk.Status.FARM)));
-        locations.add(new OwnedChunkLocationProvider(new OwnedChunk(3, 1, 0, "fb", OwnedChunk.Status.FARM)));
-
-        locations.add(new OwnedChunkLocationProvider(new OwnedChunk(0, 2, 0, "psuchtak", OwnedChunk.Status.TRANSPORT)));
-        locations.add(new OwnedChunkLocationProvider(new OwnedChunk(1, 2, 0, "DirtyFaced", OwnedChunk.Status.TRANSPORT)));
-        locations.add(new OwnedChunkLocationProvider(new OwnedChunk(2, 2, 0, "RainDrop_x", OwnedChunk.Status.TRANSPORT)));
-        locations.add(new OwnedChunkLocationProvider(new OwnedChunk(3, 2, 0, "fb", OwnedChunk.Status.TRANSPORT)));
+        for (int chunkX = minOwnedChunkX; chunkX <= maxOwnedChunkX; chunkX++) {
+            for (int chunkZ = minOwnedChunkZ; chunkZ <= maxOwnedChunkZ; chunkZ++) {
+                final OwnedChunk ownedChunk = chunkStorage.getElementAtChunk(chunkX, chunkZ);
+                if (ownedChunk != null) {
+                    locations.add(new OwnedChunkLocationProvider(ownedChunk));
+                }
+            }
+        }
 
         return locations;
     }
