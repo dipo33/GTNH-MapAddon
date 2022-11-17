20947d1de1b6d7c0f8fb42dc0e46a8428b927b00
Matej Dipčár <mato.dipcar@gmail.com>
2022-11-17 20:36:23 +0100

    Move ChunkBuy related methods to separate class

--- src/main/java/io/github/dipo33/gtmapaddon/command/ChunkCommand.java
+++ src/main/java/io/github/dipo33/gtmapaddon/command/ChunkCommand.java
@@ -5,23 +5,16 @@ import io.github.dipo33.gtmapaddon.GTMapAddonMod;
 import io.github.dipo33.gtmapaddon.GeneralUtils;
 import io.github.dipo33.gtmapaddon.command.factory.CommandFactory;
 import io.github.dipo33.gtmapaddon.command.factory.argument.ArgumentList;
-import io.github.dipo33.gtmapaddon.command.factory.exception.CommandException;
-import io.github.dipo33.gtmapaddon.command.factory.exception.CommandProcessException;
 import io.github.dipo33.gtmapaddon.command.factory.subcommand.SubCommandFactory;
 import io.github.dipo33.gtmapaddon.compat.MoneyModWrapper;
 import io.github.dipo33.gtmapaddon.data.entity.MinedChunk;
-import io.github.dipo33.gtmapaddon.data.entity.OwnedChunk;
 import io.github.dipo33.gtmapaddon.data.entity.OwnedChunk.Status;
 import io.github.dipo33.gtmapaddon.network.AddMinedChunkMessage;
-import io.github.dipo33.gtmapaddon.network.BoughtChunkMessage;
 import io.github.dipo33.gtmapaddon.storage.DataCache;
-import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChunkCoordinates;
 import net.minecraftforge.common.DimensionManager;
-import sk.dipo.money.item.MoneyItems;
 
 public class ChunkCommand {
 
@@ -33,7 +26,7 @@ public class ChunkCommand {
                                         .createSubCommand("buy")
                                         .addEnumArgument("type", Status.class).build()
                                         .addStringArgument("pinCode").setExpectedLength(4).build()
-                                        .build(ChunkCommand::buyCommand)
+                                        .build(ChunkBuyCommand::buyCommand)
                             ).addSubCommand(SubCommandFactory
                                         .createSubCommand("mark-as-mined")
                                         .addOfflinePlayerArgument("mined-by")
@@ -59,82 +52,6 @@ public class ChunkCommand {
                                         ).build()
                             ).build()
                 );
-
-    private static int getPrice(int dimensionId, int chunkX, int chunkZ, Status status) throws CommandException {
-        final String dimensionName = DimensionManager.getWorld(dimensionId).provider.getDimensionName();
-        final int price = DataCache.PRIZE_LIST.getPrice(dimensionId, status);
-        if (price < 0) {
-            throw new CommandProcessException("chunkPriceNotSet", status.getName(), chunkX, chunkZ, dimensionName);
-        }
-
-        return price;
-    }
-
-    private static ItemStack getCreditCard(ICommandSender sender) throws CommandException {
-        final ItemStack creditCard = CommandBase.getCommandSenderAsPlayer(sender).getHeldItem();
-        if (creditCard == null || creditCard.getItem() != MoneyItems.creditCard) {
-            throw new CommandProcessException("useCreditCardNotHeld");
-        }
-
-        return creditCard;
-    }
-
-    private static int checkUpgradePrice(ICommandSender sender, OwnedChunk ownedChunk, OwnedChunk previouslyOwnedChunk, int price) throws CommandException {
-        final String dimensionName = DimensionManager.getWorld(ownedChunk.getDimensionId()).provider.getDimensionName();
-
-        if (previouslyOwnedChunk != null) {
-            if (!previouslyOwnedChunk.getOwner().equalsIgnoreCase(sender.getCommandSenderName())) {
-                throw new CommandProcessException("chunkAlreadyOwnedBy", ownedChunk.getStatus().getName(),
-                        ownedChunk.getChunkX(), ownedChunk.getChunkZ(), dimensionName, previouslyOwnedChunk.getOwner()
-                );
-            }
-
-            int paidPrice = DataCache.PRIZE_LIST.getPrice(ownedChunk.getDimensionId(), previouslyOwnedChunk.getStatus());
-            price -= paidPrice;
-            if (price <= 0) {
-                throw new CommandProcessException("chunkAlreadyOwned", ownedChunk.getStatus().getName(),
-                        ownedChunk.getChunkX(), ownedChunk.getChunkZ(), dimensionName
-                );
-            }
-        }
-
-        return price;
-    }
-
-    private static void buyCommand(ArgumentList arguments, ICommandSender sender) throws CommandException {
-        final Status status = arguments.getEnum(0);
-        final String pinCode = arguments.getString(1);
-
-        final ChunkCoordinates coordinates = sender.getPlayerCoordinates();
-        final int chunkX = Utils.coordBlockToChunk(coordinates.posX);
-        final int chunkZ = Utils.coordBlockToChunk(coordinates.posZ);
-        final int dimensionId = sender.getEntityWorld().provider.dimensionId;
-        final String owner = sender.getCommandSenderName();
-
-        final ItemStack creditCard = getCreditCard(sender);
-        final int price = getPrice(dimensionId, chunkX, chunkZ, status);
-
-        final OwnedChunk ownedChunk = new OwnedChunk(chunkX, chunkZ, dimensionId, owner, status);
-        final OwnedChunk previouslyOwnedChunk = DataCache.OWNED_CHUNKS_STORAGE.get(dimensionId, chunkX, chunkZ);
-        final int newPrice = checkUpgradePrice(sender, ownedChunk, previouslyOwnedChunk, price);
-
-        final String priceString = MoneyModWrapper.priceToString(newPrice);
-        MoneyModWrapper.chargeMoney(creditCard, pinCode, newPrice);
-        DataCache.OWNED_CHUNKS_STORAGE.put(dimensionId, chunkX, chunkZ, ownedChunk);
-        GTMapAddonMod.NETWORK_CHANNEL.sendToAll(new BoughtChunkMessage(ownedChunk));
-        DataCache.OWNED_CHUNKS_SERIALIZER.save();
-
-        final String dimensionName = DimensionManager.getWorld(dimensionId).provider.getDimensionName();
-        if (price != newPrice) {
-            GeneralUtils.sendFormattedText(sender, "dipogtmapaddon.command.chunkUpgraded",
-                    previouslyOwnedChunk.getStatus().getName(), status.getName(), chunkX, chunkZ, dimensionName, priceString
-            );
-        } else {
-            GeneralUtils.sendFormattedText(sender, "dipogtmapaddon.command.chunkBought",
-                    status.getName(), chunkX, chunkZ, dimensionName, priceString
-            );
-        }
-    }
 
     private static void markAsMined(ArgumentList arguments, ICommandSender sender) {
         final ChunkCoordinates coordinates = sender.getPlayerCoordinates();
