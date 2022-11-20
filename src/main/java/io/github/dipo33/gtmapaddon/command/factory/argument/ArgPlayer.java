ccb3029d116de8247b773887f14bc774048759d8
Matej Dipčár <492666@mail.muni.cz>
2022-11-21 00:23:56 +0100

    Fix ArgPlayer

--- src/main/java/io/github/dipo33/gtmapaddon/command/factory/argument/ArgPlayer.java
+++ src/main/java/io/github/dipo33/gtmapaddon/command/factory/argument/ArgPlayer.java
@@ -3,8 +3,9 @@ package io.github.dipo33.gtmapaddon.command.factory.argument;
 import io.github.dipo33.gtmapaddon.command.factory.exception.CommandException;
 import io.github.dipo33.gtmapaddon.command.factory.exception.CommandParseException;
 import io.github.dipo33.gtmapaddon.command.factory.subcommand.WithArguments;
+import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
-import net.minecraft.command.PlayerSelector;
+import net.minecraft.command.PlayerNotFoundException;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import scala.actors.threadpool.Arrays;
@@ -19,12 +20,16 @@ public class ArgPlayer extends Argument<EntityPlayerMP> {
 
     @Override
     public EntityPlayerMP parse(String value, ICommandSender sender) throws CommandException {
-        final EntityPlayerMP player = PlayerSelector.matchOnePlayer(sender, value);
-        if (player == null) {
+        try {
+            final EntityPlayerMP player = CommandBase.getPlayer(sender, value);
+            if (player == null) {
+                throw new CommandParseException("parsePlayer", value);
+            }
+
+            return player;
+        } catch (PlayerNotFoundException e) {
             throw new CommandParseException("parsePlayer", value);
         }
-
-        return player;
     }
 
     @Override
